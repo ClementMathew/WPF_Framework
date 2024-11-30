c59ed0f22c9a830baf2ba5947a8f12462f146e61
Clement Mathew <clement.mathew@quest-global.com>
2024-11-30 13:44:58 +0530

    notepad lines number 😊

--- Notepad_Application/ViewModels/MainWindowViewModel.cs
+++ Notepad_Application/ViewModels/MainWindowViewModel.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Notepad.ViewModels
@@ -12,6 +13,8 @@ namespace Notepad.ViewModels
     {
         private string _content;
         private string _filePath;
+        private int _lineCount;
+        private int _wordCount;
 
         public string Content
         {
@@ -20,6 +23,7 @@ namespace Notepad.ViewModels
             {
                 _content = value;
                 OnPropertyChanged();
+                UpdateCounts();
             }
         }
 
@@ -31,6 +35,32 @@ namespace Notepad.ViewModels
                 _filePath = value;
                 OnPropertyChanged();
             }
+        }
+
+        public int LineCount
+        {
+            get => _lineCount;
+            private set
+            {
+                _lineCount = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int WordCount
+        {
+            get => _wordCount;
+            set
+            {
+                _wordCount = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void UpdateCounts()
+        {
+            LineCount = string.IsNullOrEmpty(Content) ? 0 : Content.Split('\n').Length;
+            WordCount = string.IsNullOrEmpty(Content) ? 0 : Regex.Matches(Content,@"\b\S+\b").Count;
         }
     }
 }
